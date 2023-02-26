7990586bdea595151f7cd55cabf5d57baf2b9454
Alexander Braml <braml11@ads.uni-passau.de>
2023-02-26 21:25:13 +0100

    Add WebDAV sample

---
+++ nextcloud/get_events.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, time
+import caldav
+
+url = ""
+today  = datetime.combine(datetime.today(), time(0,0))
+
+client = caldav.DAVClient(url)
+principal = client.principal()
+calendars = principal.calendars()
+
+if len(calendars) > 0:
+  calendar = calendars[0]
+  events = calendar.date_search(today, datetime.combine(today, time(23,59,59,59)))
+
+  if len(events) == 0:
+    print("No events today!")
+  else:
+    print("Total {num_events} events:".format(num_events=len(events)))
+
+    for event in events:
+      event.load()
+      e = event.instance.vevent
+      print(e)
