b6f1a570ecd0e26456c051f207277bdef35fd055
Alexander Braml <braml@fim.uni-passau.de>
2023-03-04 23:40:46 +0100

    Update Sample

--- src/SampleSyncer.py
+++ src/SampleSyncer.py
@@ -1,11 +1,11 @@
 import datetime
 
+from src.CaldavProvider import CaldavProvider
+from src.GCalProvider import GCalProvider
 from src.Sync import SyncJob, Syncer
 
 if __name__ == '__main__':
     from env import nc_url, nc_to_g_source, nc_to_g_target, g_to_nc_source, g_to_nc_target
-    from CaldavProvider import CaldavProvider
-    from GCalProvider import GCalProvider
 
     nc_provider = CaldavProvider(nc_url)
     g_provider = GCalProvider()
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     syncer = Syncer()
     syncer.register(
         SyncJob(nc_provider, g_provider, nc_to_g_source, nc_to_g_target, datetime.datetime.today(),
-                datetime.datetime.today() + datetime.timedelta(days=1)))
+                datetime.datetime.today() + datetime.timedelta(days=10)))
     syncer.register(SyncJob(g_provider, nc_provider, g_to_nc_source, g_to_nc_target, datetime.datetime.today(),
-                            datetime.datetime.today() + datetime.timedelta(days=1)))
+                            datetime.datetime.today() + datetime.timedelta(days=10)))
     syncer.sync()
