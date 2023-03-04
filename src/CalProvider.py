0774593bc53e4edeb099ca702b6d0ca856f5ede4
Alexander Braml <braml@fim.uni-passau.de>
2023-03-04 08:39:07 +0100

    Create methods to batch execute an operation

--- src/CalProvider.py
+++ src/CalProvider.py
@@ -8,16 +8,27 @@ from src.Event import Event, Reminder
 class CalProvider(ABC):
 
     @abstractmethod
-    def get_day(self, day: datetime.datetime) -> List[Event]:
+    def get_day(self, cal: str, day: datetime.datetime) -> List[Event]:
         pass
 
     @abstractmethod
-    def create_event(self, ev: Event) -> None:
+    def create_events(self, cal: str, events: List[Event]) -> None:
+        for event in events:
+            self.create_event(cal, event)
+
+    @abstractmethod
+    def create_event(self, cal: str, event: Event) -> None:
         pass
 
     @abstractmethod
-    def delete_event(self, ev: Event) -> None:
-        pass
+    def delete_events(self, cal: str, events: List[Event]) -> None:
+        for event in events:
+            self.delete_event(cal, event)
+
+    @abstractmethod
+    def delete_event(self, cal: str, event: Event) -> None:
+        if type(event.raw) != Any:
+            pass
 
     @abstractmethod
     def parse_event(self, raw_event: Any) -> Event:
