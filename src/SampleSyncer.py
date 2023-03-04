207b5ce9bf1209ba43a5ea73cf78d3e87d24d802
Alexander Braml <braml@fim.uni-passau.de>
2023-03-04 23:47:12 +0100

    Update Sample to use sys argument for days to be synced

--- src/SampleSyncer.py
+++ src/SampleSyncer.py
@@ -1,19 +1,25 @@
 import datetime
+import sys
 
 from src.CaldavProvider import CaldavProvider
 from src.GCalProvider import GCalProvider
 from src.Sync import SyncJob, Syncer
+from src.env import nc_url, nc_to_g_source, nc_to_go_target, go_to_nc_source, go_to_nc_target
 
 if __name__ == '__main__':
-    from env import nc_url, nc_to_g_source, nc_to_g_target, g_to_nc_source, g_to_nc_target
+    if len(sys.argv) < 2:
+        raise TypeError('You have to provide the number of days to be synced!')
+    elif not sys.argv[1].isdigit():
+        raise ValueError('You amount of days to be synced is not a number!')
+
+    days = int(sys.argv[1])
+    today = datetime.datetime.today()
+    last_day = today + datetime.timedelta(days=days)
 
     nc_provider = CaldavProvider(nc_url)
-    g_provider = GCalProvider()
+    go_provider = GCalProvider()
 
     syncer = Syncer()
-    syncer.register(
-        SyncJob(nc_provider, g_provider, nc_to_g_source, nc_to_g_target, datetime.datetime.today(),
-                datetime.datetime.today() + datetime.timedelta(days=10)))
-    syncer.register(SyncJob(g_provider, nc_provider, g_to_nc_source, g_to_nc_target, datetime.datetime.today(),
-                            datetime.datetime.today() + datetime.timedelta(days=10)))
+    syncer.register(SyncJob(nc_provider, go_provider, nc_to_g_source, nc_to_go_target, today, last_day))
+    syncer.register(SyncJob(go_provider, nc_provider, go_to_nc_source, go_to_nc_target, today, last_day))
     syncer.sync()
