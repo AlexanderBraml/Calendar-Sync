5e2eeae418a89d8f98000e2254eda1f930d490e8
Alexander Braml <braml@fim.uni-passau.de>
2023-03-04 22:26:52 +0100

    Add SampleSyncer

---
+++ src/SampleSyncer.py
@@ -0,0 +1,19 @@
+import datetime
+
+from src.Sync import SyncJob, Syncer
+
+if __name__ == '__main__':
+    from env import nc_url, g_cal_id, nc_to_g_source, nc_to_g_target, g_to_nc_source, g_to_nc_target
+    from CaldavProvider import CaldavProvider
+    from GCalProvider import GCalProvider
+
+    nc_provider = CaldavProvider(nc_url)
+    g_provider = GCalProvider()
+
+    syncer = Syncer()
+    syncer.register(
+        SyncJob(nc_provider, g_provider, nc_to_g_source, nc_to_g_target, datetime.datetime.today(),
+                datetime.datetime.today() + datetime.timedelta(days=1)))
+    syncer.register(SyncJob(g_provider, nc_provider, g_to_nc_source, g_to_nc_target, datetime.datetime.today(),
+                            datetime.datetime.today() + datetime.timedelta(days=1)))
+    syncer.sync()
