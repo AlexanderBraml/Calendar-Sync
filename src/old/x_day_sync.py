669e02b819e681ec5857c1ae0313a2cac2111d05
Alexander Braml <braml@fim.uni-passau.de>
2023-03-04 22:28:23 +0100

    Remove old scripts

--- src/old/x_day_sync.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from sync import sync_to
-
-if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        days = int(sys.argv[1])
-        sync_to(days, "go")
-        sync_to(days, "nc")
