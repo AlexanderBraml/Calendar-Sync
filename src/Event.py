299d98361df3202f62d058109a34b579b86b6509
Alexander Braml <braml@fim.uni-passau.de>
2023-03-04 08:39:28 +0100

    Implement custom equals method

--- src/Event.py
+++ src/Event.py
@@ -17,3 +17,12 @@ class Event:
     end_time: datetime.datetime
     reminder: List[Reminder]
     raw: Any
+
+    def short_str(self):
+        return f'Event[{self.summary}, {self.start_time}, {self.end_time}, {self.is_all_day}]'
+
+    def __eq__(self, other) -> bool:
+        if type(other) != Event:
+            return False
+
+        return self.summary == other.summary and self.start_time == other.start_time and self.end_time == other.end_time
