8298ebed511e7df0159773d449eb6c55de267fbc
Alexander Braml <braml@fim.uni-passau.de>
2023-03-10 12:48:19 +0100

    Add logging to SyncJob and Syncer

--- src/Sync.py
+++ src/Sync.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 from dataclasses import dataclass
 from typing import List, Set
 
@@ -12,6 +13,9 @@ def days_equal(day1: Set[Event], day2: Set[Event]) -> bool:
     return day1 == day2
 
 
+log = logging.getLogger('calendar_sync')
+
+
 @dataclass
 class SyncJob:
     source_cal_provider: CalProvider
@@ -22,15 +26,22 @@ class SyncJob:
     end_date: datetime.datetime
 
     def sync(self) -> None:
+        log.info(f'Syncing {self.source_cal_provider} ({self.source_cal}) '
+                 f'-> {self.target_cal_provider} ({self.target_cal})')
         amount_of_days: int = (self.end_date - self.start_date).days
         for delta in range(amount_of_days + 1):
             day = self.start_date + datetime.timedelta(delta)
+            log.info(f'Syncing day {day.date()}')
 
             source_day: List[Event] = self.source_cal_provider.get_day(day, self.source_cal)
             target_day: List[Event] = self.target_cal_provider.get_day(day, [self.target_cal])
+            log.debug(f'Source day: {source_day}\nTarget day: {target_day}')
             if not days_equal(set(source_day), set(target_day)):
+                log.info('Source day and target day are not equal, syncing them')
                 self.target_cal_provider.delete_events(self.target_cal, target_day)
+                log.debug('Successfully deleted all events from target day in target calendar')
                 self.target_cal_provider.create_events(self.target_cal, source_day)
+                log.debug('Successfully created all events from source day in target calendar')
 
 
 class Syncer:
@@ -40,7 +51,10 @@ class Syncer:
 
     def register(self, sync_job: SyncJob) -> None:
         self.sync_jobs.append(sync_job)
+        log.debug(f'Added {sync_job} to list of sync jobs')
 
     def sync(self) -> None:
         for job in self.sync_jobs:
+            log.debug(f'Syncing job {job}')
             job.sync()
+            log.info('Successfully executed sync job')
