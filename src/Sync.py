15a33b4f94efafb93d82bd7072b4d740098af877
Alexander Braml <braml@fim.uni-passau.de>
2023-03-10 08:51:23 +0100

    Use a set for comparing days, because order is not relevant

--- src/Sync.py
+++ src/Sync.py
@@ -1,18 +1,15 @@
 import datetime
 from dataclasses import dataclass
-from typing import List
+from typing import List, Set
 
 from src.CalProvider import CalProvider
 from src.Event import Event
 
 
-def days_equal(day1: List[Event], day2: List[Event]) -> bool:
+def days_equal(day1: Set[Event], day2: Set[Event]) -> bool:
     if len(day1) != len(day2):
         return False
-    for ev1, ev2 in zip(day1, day2):
-        if ev1 != ev2:
-            return False
-    return True
+    return day1 == day2
 
 
 @dataclass
@@ -28,9 +25,13 @@ class SyncJob:
         amount_of_days: int = (self.end_date - self.start_date).days
         for delta in range(amount_of_days + 1):
             day = self.start_date + datetime.timedelta(delta)
+
+            if day == datetime.datetime(2023, 3, 11):
+                pass
+
             source_day: List[Event] = self.source_cal_provider.get_day(day, self.source_cal)
             target_day: List[Event] = self.target_cal_provider.get_day(day, [self.target_cal])
-            if not days_equal(source_day, target_day):
+            if not days_equal(set(source_day), set(target_day)):
                 self.target_cal_provider.delete_events(self.target_cal, target_day)
                 self.target_cal_provider.create_events(self.target_cal, source_day)
 
